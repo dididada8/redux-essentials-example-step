8b9bd3f3d62ce2d59fee4193cd0113c383eab1bb
hzl2018 <hzhonglong2018@gmail.com>
2022-07-01 11:47:43 +0800

    first commit

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export const Navbar = () => {
     return (
         <nav>
             <section>
-                <h1>Redux Essentials Example</h1>
+                <h1>Redux 循序渐进示例</h1>
 
                 <div className="navContent">
-                    <div className="navLinks"></div>
+                    <div className="navLinks">
+                        <Link to="/">帖子列表</Link>
+                    </div>
                 </div>
             </section>
         </nav>
